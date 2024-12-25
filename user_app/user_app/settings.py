898f32034caaa74caab3fa819abf6b90e53e6641
Acer@vincepagaran <kaitopagaran@gmail.com>
2024-12-25 15:16:23 +0800

    fix: error settings

--- user_app/user_app/settings.py
+++ user_app/user_app/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_project',
         'USER': 'postgres',
-        'PASSWORD': 'reymart',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',  # Set to your PostgreSQL host
         'PORT': '5432',  # Default PostgreSQL port
     }
